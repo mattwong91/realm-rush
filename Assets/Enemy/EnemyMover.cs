e70d4b4345e9c361f00b61563b478e83eaf1ea7c
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-08 23:31:12 -0600

    Enemy looks at direction and has tuneable speed

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
   [SerializeField] List<Waypoint> path = new List<Waypoint>();
-  [SerializeField] float waitTime = 1f;
+  [SerializeField][Range(0f, 5f)] float speed = 0.5f;
 
   void Start()
   {
@@ -20,9 +20,11 @@ public class EnemyMover : MonoBehaviour
       Vector3 endPosition = waypoint.transform.position;
       float travelPercent = 0f;
 
+      transform.LookAt(endPosition);
+
       while (travelPercent < 1f)
       {
-        travelPercent += Time.deltaTime;
+        travelPercent += Time.deltaTime * speed;
         transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
         yield return new WaitForEndOfFrame();
       }

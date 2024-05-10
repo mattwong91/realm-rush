71506eaa1bb2653647a27d9832a10a7cdd4ef30a
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-09 21:14:06 -0600

    Spawn enemy every second

---
+++ Assets/Enemy/ObjectPool.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+  [SerializeField] GameObject enemyPrefab;
+  [SerializeField] float spawnTimer = 1f;
+
+  void Start()
+  {
+    StartCoroutine(SpawnEnemy());
+  }
+
+  IEnumerator SpawnEnemy()
+  {
+    while (true)
+    {
+      Instantiate(enemyPrefab, transform);
+      yield return new WaitForSeconds(spawnTimer);
+    }
+  }
+}

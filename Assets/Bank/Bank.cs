86f005872d956ede23ef198ca3a57ace7de7e9ce
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-10 22:38:10 -0600

    Add Gold UI Text

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,24 +10,34 @@ public class Bank : MonoBehaviour
   int currentBalance;
   public int CurrentBalance { get { return currentBalance; } }
 
+  [SerializeField] TextMeshProUGUI displayBalance;
+
   void Awake()
   {
     currentBalance = startingBalance;
+    UpdateDisplay();
   }
 
   public void Deposit(int amount)
   {
     currentBalance += Mathf.Abs(amount);
+    UpdateDisplay();
   }
 
   public void Withdraw(int amount)
   {
     currentBalance -= Mathf.Abs(amount);
+    UpdateDisplay();
 
     if (currentBalance < 0)
     {
       ReloadScene();
     }
+  }
+
+  void UpdateDisplay()
+  {
+    displayBalance.text = $"Gold: {currentBalance}";
   }
 
   void ReloadScene()

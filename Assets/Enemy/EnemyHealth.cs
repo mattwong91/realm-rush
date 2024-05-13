f1a88a3d95b90bb54bb2fbb1459ff268cb24a9fa
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-13 17:15:07 -0600

    Increase difficulty of enemies after defeat. Added dependency

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))] //NOTE creates a dependency on Enemy script/class
 public class EnemyHealth : MonoBehaviour
 {
   [SerializeField] int maxHp = 5;
+
+  [Tooltip("Adds amount to maxHP when enemy is defeated")]
+  [SerializeField] int difficultyRamp = 1;
+
   int currentHp = 0;
 
   Enemy enemy;
@@ -25,6 +30,7 @@ public class EnemyHealth : MonoBehaviour
     if (currentHp < 1)
     {
       gameObject.SetActive(false);
+      maxHp += difficultyRamp;
       enemy.RewardGold();
     }
   }

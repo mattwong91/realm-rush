fc778db26a04d43ae7e5be38880e966adcd8fbe6
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-14 02:56:37 -0600

    Init Node System pure C# class

---
+++ Assets/Pathfinding/GridManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+  [SerializeField] Node node;
+  void Start()
+  {
+    Debug.Log(node.coordinates);
+    Debug.Log(node.isWalkable);
+  }
+
+  void Update()
+  {
+
+  }
+}

b8801b58f484deab42913fee012ab493d5192641
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-14 12:37:02 -0600

    Grid System Init

--- Assets/Pathfinding/GridManager.cs
+++ Assets/Pathfinding/GridManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-  [SerializeField] Node node;
-  void Start()
+  [SerializeField] Vector2Int gridSize;
+  Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+  void Awake()
   {
-    Debug.Log(node.coordinates);
-    Debug.Log(node.isWalkable);
+    CreateGrid();
   }
 
-  void Update()
+  void CreateGrid()
   {
-
+    for (int x = 0; x < gridSize.x; x++)
+    {
+      for (int y = 0; y < gridSize.y; y++)
+      {
+        Vector2Int coordinates = new Vector2Int(x, y);
+        grid.Add(coordinates, new Node(coordinates, true));
+      }
+    }
   }
 }

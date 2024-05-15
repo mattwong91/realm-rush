07143a80d749af6f09a9f542efbd5cd571774c41
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-15 02:03:17 -0600

    explore neighbor nodes

--- Assets/Pathfinding/Pathfinder.cs
+++ Assets/Pathfinding/Pathfinder.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField] Node currentSearchNode;
+  Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+  GridManager gridManager;
+  Dictionary<Vector2Int, Node> grid;
 
-    // Update is called once per frame
-    void Update()
+  void Awake()
+  {
+    gridManager = FindObjectOfType<GridManager>();
+    if (gridManager != null)
     {
-        
+      grid = gridManager.Grid;
     }
+  }
+
+  void Start()
+  {
+    ExploreNeighbors();
+  }
+
+  void ExploreNeighbors()
+  {
+    List<Node> neighbors = new List<Node>();
+
+    foreach (Vector2Int direction in directions)
+    {
+      Vector2Int neighborCoordinates = currentSearchNode.coordinates + direction;
+
+      if (grid.ContainsKey(neighborCoordinates))
+      {
+        neighbors.Add(grid[neighborCoordinates]);
+
+        // TODO Remove after testing
+        grid[neighborCoordinates].isExplored = true;
+        grid[currentSearchNode.coordinates].isPath = true;
+      }
+    }
+  }
+
 }

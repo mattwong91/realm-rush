9c9d52ba983aed30db6119341badbae2dcec75a2
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-15 01:21:11 -0600

    Redo grid color for upcoming pathfinding

--- Assets/Tiles/ComponentLabeler.cs
+++ Assets/Tiles/ComponentLabeler.cs
@@ -9,17 +9,19 @@ public class ComponentLabeler : MonoBehaviour
 {
   [SerializeField] Color defaultColor = Color.black;
   [SerializeField] Color blockedColor = Color.gray;
+  [SerializeField] Color exploredColor = Color.yellow;
+  [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
 
   TextMeshPro label;
   Vector2Int coordinates = new Vector2Int();
-  Waypoint waypoint;
+  GridManager gridManager;
 
   void Awake()
   {
     label = GetComponent<TextMeshPro>();
     label.enabled = false;
+    gridManager = FindObjectOfType<GridManager>();
 
-    waypoint = GetComponentInParent<Waypoint>();
     DisplayCoordinates();
   }
 
@@ -38,13 +40,26 @@ public class ComponentLabeler : MonoBehaviour
 
   void SetLabelColor()
   {
-    if (waypoint.IsPlaceable)
+    if (gridManager == null) { return; }
+
+    Node node = gridManager.GetNode(coordinates);
+    if (node == null) { return; }
+
+    if (!node.isWalkable)
     {
-      label.color = defaultColor;
+      label.color = blockedColor;
+    }
+    else if (node.isPath)
+    {
+      label.color = pathColor;
+    }
+    else if (node.isExplored)
+    {
+      label.color = exploredColor;
     }
     else
     {
-      label.color = blockedColor;
+      label.color = defaultColor;
     }
   }
 

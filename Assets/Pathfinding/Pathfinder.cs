2f3b1d2c1d130c617924e535d7b7d657e7963841
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-05-15 16:11:23 -0600

    Implement Breadth First Search

--- Assets/Pathfinding/Pathfinder.cs
+++ Assets/Pathfinding/Pathfinder.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-  [SerializeField] Node currentSearchNode;
+  [SerializeField] Vector2Int startCoordinates;
+  [SerializeField] Vector2Int destinationCoordinates;
+
+  Node startNode;
+  Node destinationNode;
+  Node currentSearchNode;
+
+  Queue<Node> frontier = new Queue<Node>();
+  Dictionary<Vector2Int, Node> reached = new Dictionary<Vector2Int, Node>();
+
   Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
   GridManager gridManager;
   Dictionary<Vector2Int, Node> grid;
@@ -16,11 +25,14 @@ public class Pathfinder : MonoBehaviour
     {
       grid = gridManager.Grid;
     }
+
+    startNode = new Node(startCoordinates, true);
+    destinationNode = new Node(destinationCoordinates, true);
   }
 
   void Start()
   {
-    ExploreNeighbors();
+    BreadthFirstSearch();
   }
 
   void ExploreNeighbors()
@@ -34,10 +46,34 @@ public class Pathfinder : MonoBehaviour
       if (grid.ContainsKey(neighborCoordinates))
       {
         neighbors.Add(grid[neighborCoordinates]);
+      }
+    }
 
-        // TODO Remove after testing
-        grid[neighborCoordinates].isExplored = true;
-        grid[currentSearchNode.coordinates].isPath = true;
+    foreach (Node neighbor in neighbors)
+    {
+      if (!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable)
+      {
+        reached.Add(neighbor.coordinates, neighbor);
+        frontier.Enqueue(neighbor);
+      }
+    }
+  }
+
+  void BreadthFirstSearch()
+  {
+    bool isRunning = true;
+
+    frontier.Enqueue(startNode);
+    reached.Add(startCoordinates, startNode);
+
+    while (frontier.Count > 0 && isRunning)
+    {
+      currentSearchNode = frontier.Dequeue();
+      currentSearchNode.isExplored = true;
+      ExploreNeighbors();
+      if (currentSearchNode.coordinates == destinationCoordinates)
+      {
+        isRunning = false;
       }
     }
   }
